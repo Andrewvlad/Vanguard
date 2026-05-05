ddebffa3e4ce3ea752a5f0d1648e18e565de0036
Andrewvlad <50782946+Andrewvlad@users.noreply.github.com>
2026-05-05 09:58:40 -0400

    feat: log and catch
    Added logging and error catching to the worker

--- src/consumers/worker/main.ts
+++ src/consumers/worker/main.ts
@@ -1,13 +1,22 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from '../../app.module';
+import {Logger} from '@nestjs/common';
 
 // Pattern: https://docs.nestjs.com/standalone-applications
 async function bootstrap(): Promise<void> {
-    const app = await NestFactory.createApplicationContext(AppModule);
+    const app = await NestFactory.createApplicationContext(AppModule, {
+        logger: ['error', 'warn', 'log'],
+    });
+
+    const logger = new Logger('Worker');
+    logger.log('Worker process started');
 
     // Docs: https://docs.nestjs.com/standalone-applications#terminating-phase
     await app.close();
     process.exit(0);
 }
 
-void bootstrap();
+void bootstrap().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
